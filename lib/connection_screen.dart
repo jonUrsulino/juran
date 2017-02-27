5cec6ddffa635edb8e88ce7833c6108febb1d58f
jonBear <jon.beardroid@gmail.com>
2017-02-27 00:03:55 -0300

    Adicionando ambas listas como 'scrollavel'

--- lib/connection_screen.dart
+++ lib/connection_screen.dart
@@ -12,10 +12,87 @@ class ConnectionScreen extends StatefulWidget {
   // widget (with the same key), the framework will re-use the State object
   // instead of creating a new State object.
 
+  //@override
+  //_ProxyListState createState() => new _ProxyListState();
   @override
-  _ProxyListState createState() => new _ProxyListState();
+  _TabsDemoState createState() => new _TabsDemoState();
 }
 
+class Page {
+  Page({ this.name });
+  final String name;
+  final GlobalKey<ScrollableState> scrollableKey = new GlobalKey<ScrollableState>();
+}
+
+
+class _TabsDemoState extends State<ConnectionScreen> {
+  Page _selectedPage;
+  Page first = new Page(name: 'Wifi');
+  Page second = new Page(name: 'Proxy');
+
+  Proxy _proxyCart = new Proxy(name: "Nenhum");
+
+  void _handleCartChanged(Proxy proxy, bool inCart) {
+    setState(() {
+      // When user changes what is in the cart, we need to change _proxyCart
+      // inside a setState call to trigger a rebuild. The framework then calls
+      // build, below, which updates the visual appearance of the app.
+
+      if (!inCart)
+        _proxyCart = proxy;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedPage = first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new TabBarSelection<Page>(
+        values: [first, second].toList(),
+        onChanged: (Page value) {
+          setState(() {
+            _selectedPage = value;
+          });
+        },
+        child: new Scaffold(
+            scrollableKey: _selectedPage.scrollableKey,
+            body: new TabBarView<Page>(
+                children: [first, second].map((Page page) {
+                  /*return new ScrollableList(
+                      scrollableKey: page.scrollableKey,
+                      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                      itemExtent: 272.0,
+
+                      children: _allPages[page].map((_CardData data) {
+                        return new Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: new _CardDataItem(page: page, data: data)
+                        );
+                      }).toList()
+                  );*/
+                  return new MaterialList(
+                      type: MaterialListType.oneLineWithAvatar,
+                      children: config.proxies.map((Proxy proxy) {
+                        return new ProxyListItem(
+                            proxy: proxy,
+                            inCart: _proxyCart.name == proxy.name,
+                            onCartChanged: _handleCartChanged,
+                            );
+                      }),
+                  );
+                }).toList()
+            )
+        )
+    );
+  }
+}
+
+
+// Proxy
 class _ProxyListState extends State<ConnectionScreen> {
   Proxy _proxyCart = new Proxy(name: "Nenhum");
 
